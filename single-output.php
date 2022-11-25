56cc454aa3a65aae22de15d6bb6fe8ff889814a9
TakumiOtsuki-417 <heitaku443@gmail.com>
2022-11-25 20:47:22 +0900

    h1調整
    ほぼ完成だろう

--- single-output.php
+++ single-output.php
@@ -9,28 +9,32 @@
     ?>
     <article class="article__output">
       <h1>アウトプット開始<br><?php the_title(); ?></h1>
-      <?php //四字熟語を選別する内ループ
-      $my_query = new WP_Query();
-      $args = array(
-        'post_type' => 'yoji',
-        'meta_key' => 'output',
-        'meta_value' => $outputnumber,
-        'meta_compare' => '=',
-      );
-      $my_query->query( $args ); //データ取得
-      ?>
-      <?php if( $my_query->have_posts() ): while( $my_query->have_posts() ) : $my_query->the_post();?>
-      <?php //内ループ毎に各情報を変数に渡す
-      $args = array(
-        'title' => get_the_title(),
-        'card' => get_field('card'),
-        'mean' => get_field('mean'),
-        'explanation' => get_field('explanation'),
-        'example' => get_field('example'),
+      <ul class="yojiblocks">
+        <?php //四字熟語を選別する内ループ
+        $my_query = new WP_Query();
+        $args = array(
+          'post_type' => 'yoji',
+          'meta_key' => 'output',
+          'meta_value' => $outputnumber,
+          'meta_compare' => '=',
         );
-      get_template_part('loop-yoji', '', $args);
-      ?>
-      <?php endwhile; endif; wp_reset_postdata(); ?>
+        $my_query->query( $args ); //データ取得
+        ?>
+        <?php if( $my_query->have_posts() ): while( $my_query->have_posts() ) : $my_query->the_post();?>
+        <li>
+          <?php //内ループ毎に各情報を変数に渡す
+          $args = array(
+            'title' => get_the_title(),
+            'card' => get_field('card'),
+            'mean' => get_field('mean'),
+            'explanation' => get_field('explanation'),
+            'example' => get_field('example'),
+            );
+          get_template_part('loop-yoji', '', $args);
+          ?>
+        </li>
+        <?php endwhile; endif; wp_reset_postdata(); ?>
+      </ul>
     </article>
     <aside class="aside__output">
       <?php get_sidebar(); ?>
