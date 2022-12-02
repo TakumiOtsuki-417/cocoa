4f39f4a083a7cd2f4f4c00ffcd55ac829e519165
TakumiOtsuki-417 <heitaku443@gmail.com>
2022-12-02 12:19:40 +0900

    actbox調整
    ・ボーダーカラー個性は諦め方針
    ・ルビショートコード導入
    ・ルビサイズ調整

--- functions.php
+++ functions.php
@@ -73,19 +73,19 @@ function actExampleVoice ($atts, $content = null) {
   extract(shortcode_atts( array(
     'face' => 'other3',
     'name' => '???',
-), $atts ));
+  ), $atts ));
   $content = do_shortcode (shortcode_unautop ($content));
-return '<div class="act act-'.$face.'">
-  <div class="act__face">
-    <img src="'.get_template_directory_uri().'/images/faces/'.$face.'.png" alt="">
-  </div>
-  <div class="act__content">
-    <div class="name">
-      '.$name.'
+  return '<div class="act act-'.$face.'">
+    <div class="act__face">
+      <img src="'.get_template_directory_uri().'/images/faces/'.$face.'.png" alt="">
     </div>
-    <p>'.$content.'</p>
-  </div>
-</div>';
+    <div class="act__content">
+      <div class="name">
+        '.$name.'
+      </div>
+      <p>'.$content.'</p>
+    </div>
+  </div>';
 }
 add_shortcode ('aevoice', 'actExampleVoice');
 function redSpan ($atts, $content = null) {
@@ -108,17 +108,17 @@ function objectiveText ($atts, $content = null) {
 return "<p>".$content."</p>";
 }
 add_shortcode ('otext', 'objectiveText');
+function rbSpan ($atts, $content = null) {
+  extract(shortcode_atts( array(
+    'rb' => '???',
+  ), $atts ));
+  $content = do_shortcode (shortcode_unautop ($content));
+  return '<span rb="'.$rb.'">'.$content.'</span>';
+}
+add_shortcode ('rb', 'rbSpan');
 // ■■■■■■■■■■■■■■■■■■■■■■■
-// 検索フォームのショートコード
+// シーン設定のショートコード
 // ■■■■■■■■■■■■■■■■■■■■■■■
-function wordSearch () {
-  return '<form method="get" id="yojiSearch" action="'.home_url('/').'">
-  <input type="text" name="s" id="yojiSearchInput" value="'.the_search_query().'" placeholder="カスタム投稿タイプ別検索" />
-  <input type="hidden" name="post_type" value="yoji">
-  <input type="submit" value="search" accesskey="f" />
-</form>';
-  }
-  add_shortcode ('word_search', 'wordSearch');
 function sceneSetting ($atts, $content = null) {
   extract(shortcode_atts( array(
     'stage' => 'unknown',
@@ -133,6 +133,17 @@ function sceneSetting ($atts, $content = null) {
   </div>';
   }
 add_shortcode ('scene', 'sceneSetting');
+// ■■■■■■■■■■■■■■■■■■■■■■■
+// 検索フォームのショートコード
+// ■■■■■■■■■■■■■■■■■■■■■■■
+function wordSearch () {
+  return '<form method="get" id="yojiSearch" action="'.home_url('/').'">
+  <input type="text" name="s" id="yojiSearchInput" value="'.the_search_query().'" placeholder="カスタム投稿タイプ別検索" />
+  <input type="hidden" name="post_type" value="yoji">
+  <input type="submit" value="search" accesskey="f" />
+</form>';
+  }
+  add_shortcode ('word_search', 'wordSearch');
 // ■■■■■■■■■■■■■■■■■■■■■■■
 // QUEST関連ショートコード
 // ■■■■■■■■■■■■■■■■■■■■■■■
