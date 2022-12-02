15709b5e22ddced8ee44c23e05415f82a368bbfe
TakumiOtsuki-417 <heitaku443@gmail.com>
2022-12-02 13:53:43 +0900

    カラムレイアウトマージン調整
    ・article/asideのmargin-top
    ・PCレイアウトのサイドバー調整
    ・breadcrumbのサイズ調整

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
 <form method="get" id="yojiSearch" action="<?php echo home_url('/'); ?>">
   <input required type="text" name="s" id="yojiSearchInput" value="<?php the_search_query(); ?>" placeholder="キーワードを入力"/>
   <input type="hidden" name="post_type" value="yoji">
-  <input type="submit" value="けんさく！" accesskey="f" />
+  <input type="submit" value="けんさく!" accesskey="f" />
 </form>
